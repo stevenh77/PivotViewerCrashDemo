560248720a9997735279fd84f72b439d1d0ef9af
Steven Hollidge <stevenhollidge@hotmail.com>
2013-02-08 23:22:55 +0100

    Removed unrequired code noise

--- PivotViewerDemo/MainPage.xaml.cs
+++ PivotViewerDemo/MainPage.xaml.cs
@@ -17,46 +17,6 @@ namespace PivotViewerDemo
             InitializeComponent();
 
             this.pivotViewer.ItemsSource = Factory.GetData();
-            this.pivotViewer.Loaded += pivotViewer_Loaded;
         }
-
-        void pivotViewer_Loaded(object sender, RoutedEventArgs e)
-        {
-            UIHelper.SetTimeout(5000, () => this.pivotViewer.View = this.pivotViewer.GraphView);
-
-            //this.pivotViewer.Filter = "CouponToday=GE.5_LE.10";
-        }
-
-        private void GetCommands(object sender, PivotViewerCommandsRequestedEventArgs e)
-        {
-            e.Commands.Add(new AdornerCommand(e.Item as Price));
-        }
-    }
-
-    public static class UIHelper
-    {
-        public static void SetTimeout(int milliseconds, Action func)
-        {
-            var timer = new DispatcherTimerContainingAction
-            {
-                Interval = new TimeSpan(0, 0, 0, 0, milliseconds),
-                Action = func
-            };
-            timer.Tick += _onTimeout;
-            timer.Start();
-        }
-
-        private static void _onTimeout(object sender, EventArgs arg)
-        {
-            var t = sender as DispatcherTimerContainingAction;
-            t.Stop();
-            t.Action();
-            t.Tick -= _onTimeout;
-        }
-    }
-
-    public class DispatcherTimerContainingAction : System.Windows.Threading.DispatcherTimer
-    {
-        public Action Action { get; set; }
     }
 }
--- PivotViewerDemo/Shell.xaml.cs
+++ PivotViewerDemo/Shell.xaml.cs
@@ -1,40 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Windows.Navigation;
 
 namespace PivotViewerDemo
 {
-    public partial class Shell : Page
+    public partial class Shell
     {
         public Shell()
         {
             InitializeComponent();
         }
 
-        // Executes when the user navigates to this page.
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-        }
-
         private void NavigatePivotButton_Click(object sender, RoutedEventArgs e)
         {
-            this.MainPageItem.Visibility = Visibility.Visible;
-            this.DummyPageItem.Visibility = Visibility.Collapsed;
+            MainFrame.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
         }
 
         private void NavigateDummyButton_Click(object sender, RoutedEventArgs e)
         {
-            this.MainPageItem.Visibility = Visibility.Collapsed;
-            this.DummyPageItem.Visibility = Visibility.Visible;
+            MainFrame.Navigate(new Uri("/DummyPage.xaml", UriKind.Relative));
         }
     }
 }
